446b77ed9605f5774513f33d9e6b23bf178e17c6
Bill Ni <bni@stuy.edu>
2019-02-04 16:27:12 -0500

    finished 03

--- 03_canvas/animate.js
+++ 03_canvas/animate.js
@@ -4,9 +4,10 @@
 // 2019-02-04
 
 // -----------------------------  VAR INIT  -----------------------------
-var rad = 0;           //size of the circle
+var rad = 2;           //size of the circle
 var growshrink = true; //true means circle will grow
 var anakin;            //id of animationframe
+var going = false;     //is animation running
 
 var start = document.getElementById("circle");       //start animation button
 var stop = document.getElementById("stop");          //clear canvas button
@@ -25,12 +26,14 @@ ctx.fillStyle = '#16fcf4'; //set brush color blue
 // --------------------  STOP FUNCTION  ---------------------
 var st = () => {
   cancelAnimationFrame(anakin);
+  going = false;
 }
 // ----------------------------------------------------------
 
 // --------------------  ANIMATE FUNCTION  ------------------
 var dw = (ts) => {
-  if(rad == 0 || rad == canvas.width / 2){
+  going = true;
+  if(rad <= 1 || rad >= canvas.width / 2){
     growshrink = !growshrink;
   }
 
@@ -41,7 +44,8 @@ var dw = (ts) => {
   }
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.beginPath(canvas.width / 2, canvas.height / 2, rad, 0, 2 * Math.PI);
+  ctx.beginPath();
+  ctx.arc(canvas.width / 2, canvas.height / 2, rad, 0, 2 * Math.PI);
   ctx.stroke();
   ctx.fill();
 
@@ -52,6 +56,9 @@ var dw = (ts) => {
 // ----------------------------------------------------------------------
 
 // ------------------------  EVENTLISTENER INIT  ------------------------
-start.addEventListener("click", dw); //animation button eventlistener add
+start.addEventListener("click", function(event){
+  if(!going){
+    dw()
+  }}); //animation button eventlistener add
 stop.addEventListener('click', st);  //stop button eventlistener add
 // ----------------------------------------------------------------------
