abb5fa4c4aa6fa58c7f2274637d422f916519863
Bill Ni <bni@stuy.edu>
2019-03-14 00:19:53 -0400

    start 10

---
+++ 10_svg/svg01.js
@@ -0,0 +1,29 @@
+var svg = document.getElementById("vimage");
+var clr = document.getElementById("but_clear");
+
+var i = 0;
+
+var cc = (e) => {
+  var f = e.target.getAttribute("fill");
+  if(f == "blue"){
+    e.target.setAttribute("fill", "green");
+  }else{
+    e.target.remove();
+    svg.innerHTML += '<circle id="' + i.toString() + '" cx="' + (Math.random() * 500).toString() + '" cy="' + (Math.random() * 500).toString() + '" r="20" fill="blue"/>';
+    document.getElementById(i.toString()).addEventListener("click", cc);
+    i++;
+  }
+}
+
+var dw = (e) => {
+	svg.innerHTML += '<circle id="' + i.toString() + '" cx="' + e.offsetX.toString() + '" cy="' + e.offsetY.toString() + '" r="20" fill="blue"/>';
+  document.getElementById(i.toString()).addEventListener("click", cc);
+  i++;
+}
+
+var cr = () => {
+	svg.innerHTML = "";
+}
+
+svg.addEventListener("click",dw);
+clr.addEventListener("click",cr);
