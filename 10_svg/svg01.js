3af3c41bc8b0f8b1ee70fd5cb871b53717501e4c
Bill Ni <bni@stuy.edu>
2019-04-17 10:04:31 -0400

    18

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -1,5 +1,10 @@
 var svg = document.getElementById("vimage");
 var clr = document.getElementById("but_clear");
+var mve = document.getElementById("but_move");
+
+var m = false;
+var vx = Math.random() * 10 + 10;
+var vy = Math.random() * 10 + 10;
 
 var i = 0;
 
@@ -11,7 +16,7 @@ var cc = (e) => {
   }else{
     e.target.remove();
     svg.innerHTML += '<circle class="circ" cx="' + (Math.floor(Math.random() * 500)).toString() + '" cy="' + (Math.random() * 500).toString() + '" r="20" fill="blue"/>';
-    
+
     all = document.getElementsByClassName("circ")
     for(i = 0; i < all.length; i++){
       all[i].addEventListener("click", cc);
@@ -23,13 +28,25 @@ var cc = (e) => {
 
 var dw = (e) => {
 	svg.innerHTML += '<circle class="circ" cx="' + e.offsetX.toString() + '" cy="' + e.offsetY.toString() + '" r="20" fill="blue"/>';
-  
+
   all = document.getElementsByClassName("circ")
   for(i = 0; i < all.length; i++){
     all[i].addEventListener("click", cc);
   }
-  
+
   i++;
+}
+
+var mv = () => {
+  m = !m
+
+  while(m){
+    all = document.getElementsByClassName("circ");
+    for(i = 0; i < all.length; i++){
+      all[i].setAttribute("cx", (all[i].getAttribute("cx") + vx).toString());
+      all[i].setAttribute("cy", (all[i].getAttribute("cy") + vy).toString());
+    }
+  }
 }
 
 var cr = () => {
@@ -38,3 +55,4 @@ var cr = () => {
 
 svg.addEventListener("click",dw);
 clr.addEventListener("click",cr);
+mve.addEventListener("click",mv)
